31982070b59af15be9870d329318226b0fe01eb3
sakakidai <sd.lybd1016@gmail.com>
2021-09-29 09:09:35 +0900

    fix blogs page

--- components/Blogs/BlogItem.js
+++ components/Blogs/BlogItem.js
@@ -2,29 +2,45 @@ import Image from 'next/image';
 import { MyDate } from '../UI/MyDate';
 
 const BlogItem = ({ blog }) => {
+  console.log(blog.outline);
+
   return (
     <li className='rounded overflow-hidden shadow-lg'>
       <Image
         className='w-full'
         src={blog.thumbnail}
         alt='Blog Thumbnail'
-        width={400}
-        height={225}
+        width={800}
+        height={450}
       />
+
       <div className='px-6 py-4'>
-        <div className='font-bold text-xl mb-2'>{blog.title}</div>
-        <p className='text-gray-700 text-base'>
-          {blog.idol.name}({blog.idol.age}歳)
+        <div className='font-bold text-xl mb-4'>{blog.title}</div>
+        <table className='table-auto text-gray-700 text-base mb-4'>
+          <tbody>
+            <tr>
+              <th className='pr-10'>名前</th>
+              <td>
+                {blog.idol.name}({blog.idol.age}歳)
+              </td>
+              <td></td>
+            </tr>
+            <tr>
+              <th>タイトル</th>
+              <td>{blog.pieceTitle}</td>
+            </tr>
+            <tr>
+              <th>発売日</th>
+              <td>
+                <MyDate dateString={blog.pieceReleaseOn} />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+        <p className='whitespace-pre-wrap text-gray-700 text-base'>
+          {blog.outline}
         </p>
-        <p className='text-gray-700 text-base'>
-          タイトル:
-          {blog.pieceTitle}
-        </p>
-        <p className='text-gray-700 text-base'>
-          発売日:
-          <MyDate dateString={blog.pieceReleaseOn} />
-        </p>
-        <p className='text-gray-700 text-base'>{blog.outline}</p>
       </div>
       <div className='px-6 pt-4 pb-2'>
         {blog.genreList.map((genre) => (
