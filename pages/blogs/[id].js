9b85d05a7da12eba8bc94355fb71e778f1528618
sakakidai <sd.lybd1016@gmail.com>
2021-10-02 19:08:31 +0900

    bug fix to blog detail page

--- pages/blogs/[id].js
+++ pages/blogs/[id].js
@@ -1,21 +1,15 @@
 import Layout from '../../components/Layout';
 import BlogDetail from '../../components/Blogs/BlogDetail';
 import { getAllBlogIds, getBlogData } from '../../lib/blogs';
-import { useMinBreakPoint } from '../../hooks/useBreakpoint';
 import Card from '../../components/UI/Card';
 
 const Blog = ({ blog }) => {
-  // console.log(blog);
-  const screenSizeMinLg = useMinBreakPoint('lg');
-
   return (
     <Layout title={blog.title}>
       <div className='p-5 grid grid-cols-1 md:grid-cols-9 lg:grid-cols-12 gap-2'>
-        {screenSizeMinLg && (
-          <div className='bg-red-500 grid-cols-1 md:col-span-3'>
-            <h1>サイドバー</h1>
-          </div>
-        )}
+        <div className='bg-red-500 hidden lg:col-span-3 lg:inline-block'>
+          <h1>サイドバー</h1>
+        </div>
         <Card classes='grid-cols-1 md:col-span-6 bg-white'>
           <BlogDetail blog={blog} />
         </Card>
